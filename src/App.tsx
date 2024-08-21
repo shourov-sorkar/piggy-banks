9320c120fd8e1ffebf94e71a215590f4b9b9ff20
shourov-sorkar <18103142@iubat.edu>
2024-08-22 04:55:06 +0600

    feat(font): added font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 export const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold flex items-center justify-center h-screen font-matemaise">
         Monir's Piggy Bank
       </h1>
     </div>
